446ab54a48809a6400b008cc113eff3b9d4db1c6
nankou <52984307+nankou@users.noreply.github.com>
2021-03-18 20:50:51 +0800

    chen--axios请求修改

--- src/api/system/dept.js
+++
@@ -1,9 +0,0 @@
-import {axiosD, axiosG, axiosK, axiosP} from "@/api";
-
-/**
- * @module 部门
- * */
-export const addDeptApi = data => axiosK("admin/dept/addDept", data); // 新增部门
-export const deleteDeptApi = data => axiosD("admin/dept/delDept", data); // 删除部门
-export const editDeptApi = data => axiosP("admin/dept/editDept", data); // 编辑部门
-export const treeDeptApi = data => axiosG("admin/dept/treeDept", data); // 获得部门
--- src/api/system/job.js
+++
@@ -1,9 +0,0 @@
-import {axiosD, axiosG, axiosK, axiosP} from "@/api";
-
-/**
- * @module 职业
- * */
-export const addJobApi = data => axiosK("admin/job/addJob", data); // 新增职业
-export const deleteJobApi = data => axiosD("admin/job/delJob", data); // 删除职业
-export const editJobApi = data => axiosP("admin/job/editJob", data); // 编辑职业
-export const getJobListApi = data => axiosG("admin/job/pageJob", data); // 分页获得职业列表
